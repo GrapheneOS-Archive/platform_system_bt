EuclideanOS/android_system_bt

add alloc_size attributes to the allocator
This results in expanded _FORTIFY_SOURCE coverage.

osi/include/allocator.h
@@ -37,8 +37,8 @@ extern const allocator_t allocator_calloc;
 char *osi_strdup(const char *str);
 char *osi_strndup(const char *str, size_t len);
 
-void *osi_malloc(size_t size);
-void *osi_calloc(size_t size);
+void *osi_malloc(size_t size) __attribute__((alloc_size(1)));
+void *osi_calloc(size_t size) __attribute__((alloc_size(1)));
 void osi_free(void *ptr);
 
 // Free a buffer that was previously allocated with function |osi_malloc|

GrapheneOS-Archive/platform_system_bt

osi/include/allocator.h
@@ -37,8 +37,8 @@ extern const allocator_t allocator_calloc;
 char* osi_strdup(const char* str);
 char* osi_strndup(const char* str, size_t len);
 
-void* osi_malloc(size_t size);
-void* osi_calloc(size_t size);
+void* osi_malloc(size_t size) __attribute__((alloc_size(1)));
+void* osi_calloc(size_t size) __attribute__((alloc_size(1)));
 void osi_free(void* ptr);
 
 // Free a buffer that was previously allocated with function |osi_malloc|